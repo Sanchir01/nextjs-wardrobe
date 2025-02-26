1f9c05d99d4bb76396229005ef20ef2fe9afe2de
Sanchir01 <emgushovs@mail.ru>
2025-02-26 17:24:04 +0300

    add base seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,41 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 import Header from '~/widgets/header'
 import Footer from '~/widgets/footer'
 
 export const metadata: Metadata = {
 	title: 'Системы хранения',
+	verification: {
+		yandex: '6c676881d5a0ee82'
+	},
 	description:
 		'Купите качественные системы хранения и двери-купе по выгодным ценам. Стильный дизайн и надежность!',
 	keywords:
 		'titangs, системы хранения, Системы хранения, двери купе, двери-купе, двери, купе, хранения, двери-купе купить, wardrobe',
-	robots: 'index, follow'
+	robots: 'index, follow',
+	icons: { icon: '/img/icon.ico' },
+	openGraph: {
+		title: 'Системы хранения',
+		description:
+			'Купите качественные системы хранения и двери-купе по выгодным ценам.',
+		url: 'https://system-storage.ru',
+		siteName: 'Системы хранения',
+		images: [
+			{
+				url: '/img/bghero.png',
+				width: 1200,
+				height: 630,
+				alt: 'Системы хранения - двери-купе'
+			}
+		],
+		type: 'website',
+		ttl: 20,
+		locale: 'ru'
+	}
 }
-
+export const viewport: Viewport = {
+	themeColor: '#f9f9f9'
+}
 export default function RootLayout({
 	children
 }: Readonly<{
