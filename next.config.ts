1e616b5e236151e01080f11892a0d0081709cba5
Sanchir01 <emgushovs@mail.ru>
2025-02-26 22:20:46 +0300

    add new headers

--- next.config.ts
+++ next.config.ts
@@ -24,17 +24,29 @@ const nextConfig: NextConfig = {
 					{
 						key: 'Content-Security-Policy',
 						value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://apis.google.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: https://yourcdn.com;
-              font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://api.yourdomain.com;
-              object-src 'none';
-              frame-ancestors 'none';
-              base-uri 'self';
-              form-action 'self';
-            `.replace(/\s{2,}/g, ' ')
+        default-src 'self';
+        script-src 'self' https://apis.google.com;
+        style-src 'self' https://fonts.googleapis.com;
+        img-src 'self' data: https://yourcdn.com;
+        font-src 'self' https://fonts.gstatic.com;
+        connect-src 'self' https://systen-storage.ru;
+        object-src 'none';
+        frame-ancestors 'none';
+        base-uri 'self';
+        form-action 'self';
+    `.replace(/\s{2,}/g, ' ')
+					},
+					{
+						key: 'Strict-Transport-Security',
+						value: 'max-age=31536000; includeSubDomains; preload'
+					},
+					{
+						key: 'Permissions-Policy',
+						value: 'geolocation=(), microphone=(), camera=(), payment=()'
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN'
 					}
 				]
 			}
