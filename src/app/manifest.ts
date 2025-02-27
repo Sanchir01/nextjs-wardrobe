f1c80017705c010fbdf017da40853efe84abf78c
Sanchir01 <emgushovs@mail.ru>
2025-02-27 11:06:45 +0300

    add manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		lang: 'ru-RU',
+		scope: '/',
+		name: 'Системы хранения',
+		short_name: 'Wardrobe',
+		description:
+			'Купите качественные системы хранения и двери-купе в Санкт-Петербурге по выгодным ценам',
+		start_url: '/',
+		dir: 'auto',
+		orientation: 'any',
+		display: 'standalone',
+		background_color: '#f2f2f2',
+		theme_color: '#000000',
+		icons: [
+			{
+				src: 'favicon512x512.ico',
+				sizes: '512x512',
+				type: 'image/png'
+			},
+			{
+				src: 'favicon256x256.ico',
+				sizes: '256x256',
+				type: 'image/png'
+			},
+			{
+				src: 'favicon.ico',
+				sizes: '128x128',
+				type: 'image/ico'
+			}
+		]
+	}
+}
